becba0c7679a91556308472a5bbb2a565039999f
Piper Merriam <pipermerriam@gmail.com>
2016-11-08 18:04:29 -0700

    work on using bootstrap cards

--- src/js/components/ProxyTokenUpgradeForm.jsx
+++ src/js/components/ProxyTokenUpgradeForm.jsx
@@ -90,10 +90,15 @@ export default HideIfNoWeb3(connect(mapStateToProps)(React.createClass({
   renderLocalSignatureControls() {
     if (this.canSignLocally()) {
       return (
-        <div>
-          <h4>Sign In Browser</h4>
-          <p>It looks like the account <EthereumChecksumAddress address={this.props.tokenData.owner} /> is available here in the browser.</p>
-          <button className="btn btn-primary" type="button" onClick={this.signData}>Sign Data</button>
+        <div className="card">
+          <div className="card-header">
+            Manual Signature
+          </div>
+          <div className="card-block">
+            <h4>Sign In Browser</h4>
+            <p>It looks like the account <EthereumChecksumAddress address={this.props.tokenData.owner} /> is available here in the browser.</p>
+            <button className="btn btn-primary" type="button" onClick={this.signData}>Sign Data</button>
+          </div>
         </div>
       )
     } else {
@@ -104,21 +109,31 @@ export default HideIfNoWeb3(connect(mapStateToProps)(React.createClass({
     if (this.isSigned()) {
       var web3 = this.props.web3.web3
       return (
-        <div>
-          <p>The following signature is ready to be submitted to the <code>proxyUpgrade()</code> function.</p>
-          <SyntaxHighlighter language='javascript' style={docco}>{"Signed Data:\n" + web3.toHex(this.upgradeSignedBytes()) + "\n\nSignature:\n" + this.upgradeSignature()}</SyntaxHighlighter>
+        <div className="card">
+          <div className="card-header">
+            <em>Step 2</em>: Manual Signature
+          </div>
+          <div className="card-block">
+            <p>The following signature is ready to be submitted to the <code>proxyUpgrade()</code> function.</p>
+            <SyntaxHighlighter language='javascript' style={docco}>{"Signed Data:\n" + web3.toHex(this.upgradeSignedBytes()) + "\n\nSignature:\n" + this.upgradeSignature()}</SyntaxHighlighter>
+          </div>
         </div>
       )
     } else {
       return (
-        <div>
-          <p>The following data needs to be signed by <EthereumChecksumAddress address={this.props.tokenData.owner} />.  The data is the concatenated bytes of:</p>
-          <ul>
-            <li>Token contract address: <EthereumChecksumAddress address="0xC59162bbe31b2ECa8E3d7d3401DC05f64E293f83" /></li>
-            <li>Previous owner: <EthereumChecksumAddress address={this.props.tokenData.owner} /></li>
-            <li>Owner after upgrade: <EthereumChecksumAddress address={this.upgradeTarget()} /></li>
-          </ul>
-          <SyntaxHighlighter language='javascript' style={docco}>{"Data To Sign (hex encoded):\n" + this.hexToSign()}</SyntaxHighlighter>
+        <div className="card">
+          <div className="card-header">
+            <em>Step 2</em>: Manual Signature
+          </div>
+          <div className="card-block">
+            <p>The following data needs to be signed by <EthereumChecksumAddress address={this.props.tokenData.owner} />.  The data is the concatenated bytes of:</p>
+            <ul>
+              <li>Token contract address: <EthereumChecksumAddress address="0xC59162bbe31b2ECa8E3d7d3401DC05f64E293f83" /></li>
+              <li>Previous owner: <EthereumChecksumAddress address={this.props.tokenData.owner} /></li>
+              <li>Owner after upgrade: <EthereumChecksumAddress address={this.upgradeTarget()} /></li>
+            </ul>
+            <SyntaxHighlighter language='javascript' style={docco}>{"Data To Sign (hex encoded):\n" + this.hexToSign()}</SyntaxHighlighter>
+          </div>
         </div>
       )
     }
@@ -130,9 +145,15 @@ export default HideIfNoWeb3(connect(mapStateToProps)(React.createClass({
       )
     } else {
       return (
-        <div>
-          {this.renderLocalSignatureControls()}
-          <ManualSignatureForm tokenOwner={this.props.tokenData.owner} dataHash={this.dataHash()} onSubmit={this.setUpgradeSignature} />
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <ManualSignatureForm tokenOwner={this.props.tokenData.owner} dataHash={this.dataHash()} onSubmit={this.setUpgradeSignature} />
+            </div>
+            <div className="col-sm-6">
+              {this.renderLocalSignatureControls()}
+            </div>
+          </div>
         </div>
       )
     }
@@ -153,33 +174,37 @@ export default HideIfNoWeb3(connect(mapStateToProps)(React.createClass({
 let SignatureParametersForm = reduxForm({form: 'proxy-signature-parameters'})(React.createClass({
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit}>
-        <div className="row">
-          <h4>Signature Parameters</h4>
+      <div className="card">
+        <div className="card-header">
+          <em>Step 1</em>: Signature Parameters
         </div>
-        <div className="form-group row">
-          <label htmlFor="contractAddress" className="col-sm-2 col-form-label">Contract Address</label>
-          <div className="col-sm-10">
-            <Field component="input" type="text" readOnly="true" className="form-control" name="contractAddress" placeholder="0x..."/>
-            <p className="form-text text-muted">The address of the upgraded token contract</p>
-          </div>
+        <div className="card-block">
+          <form className="container clearfix" onSubmit={this.props.handleSubmit}>
+            <div className="form-group row">
+              <label htmlFor="contractAddress" className="col-sm-2 col-form-label">Contract Address</label>
+              <div className="col-sm-10">
+                <Field component="input" type="text" readOnly="true" className="form-control" name="contractAddress" placeholder="0x..."/>
+                <p className="form-text text-muted">The address of the upgraded token contract</p>
+              </div>
+            </div>
+            <div className="form-group row">
+              <label htmlFor="currentOwner" className="col-sm-2 col-form-label">Current Owner</label>
+              <div className="col-sm-10">
+                <Field component="input" type="text" readOnly="true" className="form-control" name="currentOwner" placeholder="0x..."/>
+                <p className="form-text text-muted">The address that owns the token being upgraded in the old token contract.</p>
+              </div>
+            </div>
+            <div className="form-group row">
+              <label htmlFor="tokenOwner" className="col-sm-2 col-form-label">New Owner</label>
+              <div className="col-sm-10">
+                <Field component="input" type="text" className="form-control" name="tokenOwner" placeholder="0x..." />
+                <p className="form-text text-muted">The address that will own the token after upgrading.</p>
+              </div>
+            </div>
+            <p className="text-xs-right"><button type="submit" className="btn btn-primary">Next</button></p>
+          </form>
         </div>
-        <div className="form-group row">
-          <label htmlFor="currentOwner" className="col-sm-2 col-form-label">Current Owner</label>
-          <div className="col-sm-10">
-            <Field component="input" type="text" readOnly="true" className="form-control" name="currentOwner" placeholder="0x..."/>
-            <p className="form-text text-muted">The address that owns the token being upgraded in the old token contract.</p>
-          </div>
-        </div>
-        <div className="form-group row">
-          <label htmlFor="tokenOwner" className="col-sm-2 col-form-label">New Owner</label>
-          <div className="col-sm-10">
-            <Field component="input" type="text" className="form-control" name="tokenOwner" placeholder="0x..." />
-            <p className="form-text text-muted">The address that will own the token after upgrading.</p>
-          </div>
-        </div>
-        <button type="submit" className="btn btn-primary">Save</button>
-      </form>
+      </div>
     )
   }
 }))
@@ -188,17 +213,21 @@ let SignatureParametersForm = reduxForm({form: 'proxy-signature-parameters'})(Re
 let ManualSignatureForm = reduxForm({form: 'manual-signature'})(React.createClass({
   render() {
     return (
-      <div>
-        <h4>Sign Manually</h4>
-        <p>You can sign the data above using the <code>geth</code> console:</p>
-        <SyntaxHighlighter language='javascript' style={docco}>{'web3.eth.sign("' + this.props.tokenOwner + '", "' + this.props.dataHash + '")'}</SyntaxHighlighter>
-        <form onSubmit={this.props.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="upgradeSignature" className="col-form-label">Signature</label>
-            <Field type="text" component="input" className="form-control" name="upgradeSignature" />
-          </div>
-          <button className="btn btn-primary" type="submit">Next</button>
-        </form>
+      <div className="card">
+        <div className="card-header">
+          Manual Signature
+        </div>
+        <div className="card-block">
+          <p>You can sign the data above with the following command using the <code>geth</code> console:</p>
+          <SyntaxHighlighter language='javascript' style={docco}>{'web3.eth.sign("' + this.props.tokenOwner + '", "' + this.props.dataHash + '")'}</SyntaxHighlighter>
+          <form onSubmit={this.props.handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="upgradeSignature" className="col-form-label">Signature</label>
+              <Field type="text" component="input" className="form-control" name="upgradeSignature" />
+            </div>
+            <button className="btn btn-primary" type="submit">Save Signature</button>
+          </form>
+        </div>
       </div>
     )
   }
